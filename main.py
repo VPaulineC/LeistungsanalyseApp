81abc01880236c43a4d5c51bd8429e361cf91a1d
Holand13 <163293066+Holand13@users.noreply.github.com>
2024-04-10 13:07:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import my_functions
+import time
 
+
+if __name__ == "__main__":
+    eingabe = input("Bitte gib dein Alter ein: ")
+    Alter = int(eingabe)
+    Geschlecht = input(str("male/female? "))
+    Vorname = input(str("Vorname: "))
+    Zuname = input(str("Nachname: "))
+    Name_Experiment = "Leistungsanalyse"
+    Datum = time.strftime("%d/%m/%Y")
+    Supervisor = "Max Mustermann"
+    Thema =  "Leistung"
+    
+
+
+    Dictionary = {"Maximale Herzrate:": my_functions.estimate_max_hr(age_years=Alter,sex = Geschlecht),
+                  "Personen Angabe": my_functions.build_person(first_name=Vorname,last_name=Zuname,sex=Geschlecht,age=Alter),
+                  "Experiment:": my_functions.build_experiment(experiment_name=Name_Experiment,date=Datum,supervisor=Supervisor,subject=Thema),
+                  "Mittelwert:": calculation.calc_mean(list_of_measurements= Mittelwert)
+            }
+    print(Dictionary)
