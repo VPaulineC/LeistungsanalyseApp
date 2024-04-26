51b6124a8c14e38ee70ce2372c9ac150e44b2daf
VPaulineC <vp2765@mci4me.at>
2024-04-26 14:06:42 +0200

    Aufgabe 62

--- main.py
+++ main.py
@@ -10,23 +10,28 @@ if __name__ == "__main__":
     eingabe=input ("Gib bitte dein Alter ein: ")
     age_years = int(eingabe)
     sex = input(str("male/female? "))
-    first_name = input(str("Vorname: "))
-    last_name = input(str("Nachname: "))
+    Subject_first_name = input(str("Vorname Subject: "))
+    Subject_last_name = input(str("Nachname Subject: "))
+    dateofbirth = input(str("Gib bitte dein Geburtsdatum ein: "))
     experiment_name = input(str("Gib bitte einen Namen für das Experiment an: "))
     date = time.strftime("%d/%m/%Y")
-    supervisor = input(str("Gib bitte den Supervisor an: "))
-    subject =  input(str("Gib bitte das Subjekt an: "))
+    Supervisor_first_name = input(str("Vorname Subervisor: "))
+    Supervisor_last_name = input(str("Nachname Supervisor: "))
     
-    # Person1 und Experiment1 erstellen
-    Person1 = my_classes.Person(first_name, last_name, sex, age_years)
-    Experiment1 = my_classes.Experiment(experiment_name, date, supervisor, subject)
     
-    #Person1 und Experiment1 als Dictionarys printen
-    print(Person1.__dict__)
+    # Subject1, Supervisor1 und Experiment1 erstellen
+    Subject1 = my_classes.Subject(Subject_first_name, Subject_last_name, sex, age_years, dateofbirth)
+    Supervisor1 = my_classes.Supervisor(Supervisor_first_name, Supervisor_last_name)
+    Experiment1 = my_classes.Experiment(experiment_name, date, Supervisor1.__dict__, Subject1.__dict__)
+    
+    #Subject1 und Experiment1 als Dictionarys printen
+    print(Subject1.__dict__)
+    #print(Supervisor1.__dict__)
     print(Experiment1.__dict__)
     
-    #Dictionarys von Person1 und Experiment1 in sample.json speichern
-    my_classes.Person.save(Person1)
+    #Dictionarys von Subject1 und Experiment1 in sample.json speichern
+    my_classes.Subject.save(Subject1)
+    #my_classes.Supervisor.save(Supervisor1)
     my_classes.Experiment.save(Experiment1)
     
  
--- my_classes.py
+++ my_classes.py
@@ -3,12 +3,22 @@ import json
 class Person():
     """A simple attempt to model a person."""
     
-    def __init__(self, first_name, last_name, sex, age_years):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+
+    def save(self):
+        with open("sample.json", "a") as outfile: 
+            json.dump(self.__dict__, outfile)
+            outfile.write("\n")
+    
+# Kindklasse Subject erstellen und die entsprechenden Attribute vererben bzw. erstellen
+class Subject(Person):
+    def __init__(self, first_name, last_name, sex, age_years, dateofbirth):
+        super().__init__(first_name, last_name)
+        self.__dateofbirth = dateofbirth
         self.sex = sex
         self.age_years = age_years
-        self.estimated_max_hr = self.estimate_max_hr(age_years, sex)
 
     
     def estimate_max_hr(self, age_years, sex):
@@ -22,10 +32,22 @@ class Person():
             max_hr_bpm  = int(input("Enter maximum heart rate:"))
         return int(max_hr_bpm)
     
-    #Methode um Person() in ein Dictionary zu speichern
+               
     def save(self):
         with open("sample.json", "a") as outfile: 
             json.dump(self.__dict__, outfile)
+            outfile.write("\n")
+    
+# Kindklasse Supervisor erstellen und die entsprechenden Attribute vererben bzw. erstellen 
+class Supervisor(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+
+
+    def save(self):
+         with open("sample.json", "a") as outfile: 
+             json.dump(self.__dict__, outfile)
+             outfile.write("\n")
 
 
 class Experiment():
@@ -35,8 +57,8 @@ class Experiment():
         self.supervisor = supervisor
         self.subject = subject
 
-    #Methode um Experiment() in ein Dictionary zu speichern
+
     def save(self):
         with open("sample.json", "a") as outfile: 
             json.dump(self.__dict__, outfile)
-            
+            outfile.write("\n")
